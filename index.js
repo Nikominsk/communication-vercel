b8a5985df80aec3307e4c73b62dba463acb5083c
Nikominsk <nniko.geuenich@gmail.com>
2024-11-14 23:45:15 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
 // index.js
 const express = require('express')
+const path = require('path')
+const dotenv = require('dotenv')
+const bcrypt = require('bcrypt')
+
+dotenv.config();  // Load environment variables
+
 const app = express()
 const PORT = 4000
 
+app.use(express.static(path.join(path.resolve(), 'public')));
+app.use(express.json());
+
+
 app.get('/home', (req, res) => {
   res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.post('/request', async (req, res) => {
+
+    if(req.body == null) {
+        res.json(null);
+        return;
+    }
+
+    const { prompt, secret } = req.body;
+
+    if(!prompt || !secret) {
+        res.json(null);
+        return;
+    }
+
+
+   const isMatch = await bcrypt.compare(secret, process.env.HELLO);
+    if (!isMatch) {
+        return res.status(401).json({ message: "Unauthorized: Invalid secret code." });
+    }
+
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.OPEN_AI}`,
+        },
+        body: JSON.stringify({
+        model: 'gpt-4o-mini',
+        messages: [{ role: 'user', content: prompt }],
+        stream: false,
+        }),
+    });
+  
+    const data = await response.json();
+    res.status(200).json(data);
 })
 
 app.listen(PORT, () => {
